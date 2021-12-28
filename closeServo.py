be2a70b115609682ae7e18c8142948a66e648f61
Simon Moinard <simmoinard@gmail.com>
2021-12-28 11:15:58 +0100

    Create closeServo.py

---
+++ closeServo.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(7)  #Close Servo
+time.sleep(0.5)
+
+p.stop()
+GPIO.cleanup()
+
